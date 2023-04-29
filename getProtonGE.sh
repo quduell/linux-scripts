e85d501dac31d69f33ea3af7615f55b984105f21
quduell <118320797+quduell@users.noreply.github.com>
2023-04-29 17:59:57 +0000

    Add files via upload
    the first version of this script is a travesty lol

--- getProtonGE.sh
+++ getProtonGE.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 clear
+dir=~/.steam/root/compatibilitytools.d
 echo -n "Enter link here dumbass: "
 read link
-wget $link -P ~/Desktop
-tar -xf GE-Proton*
-rm GE-Proton*.tar.gz
-rm -rf ~/.steam/root/compatibilitytools.d/*
-mv GE-Proton* ~/.steam/root/compatibilitytools.d/
+wget $link -P ~/Desktop/
+if [ -d "$dir" ]; then
+	rm -rf $dir/*
+else
+	mkdir -p $dir
+fi
+tar -xf ~/Desktop/GE-Proton*.tar.gz -C $dir/
+rm ~/Desktop/GE-Proton*

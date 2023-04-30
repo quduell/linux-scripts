5e31cadfe1f56fc031d2737e5fbd17cfe2e5e14d
quduell <118320797+quduell@users.noreply.github.com>
2023-04-30 15:11:25 +0000

    Add files via upload
    changed ~/Desktop to /tmp since more desktop linux users have a /tmp directory than a ~/Desktop directory

--- getProtonGE.sh
+++ getProtonGE.sh
@@ -3,11 +3,11 @@ clear
 dir=~/.steam/root/compatibilitytools.d
 echo -n "Enter link here dumbass: "
 read link
-wget $link -P ~/Desktop/
+wget $link -P /tmp/ 
 if [ -d "$dir" ]; then
 	rm -rf $dir/*
 else
 	mkdir -p $dir
 fi
-tar -xf ~/Desktop/GE-Proton*.tar.gz -C $dir/
-rm ~/Desktop/GE-Proton*
+tar -xf /tmp/GE-Proton*.tar.gz -C $dir/
+rm /tmp/GE-Proton*

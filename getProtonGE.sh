c88ad977ef0d90511011c77266ad8941e9cba638
quduell <118320797+quduell@users.noreply.github.com>
2023-09-14 02:12:15 +0000

    Add files via upload

--- getProtonGE.sh
+++ getProtonGE.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
-clear
 link=$(curl -s https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest | grep browser_download_url | cut -d\" -f4 | grep -e .tar.gz)
-read -p "Did you want to download the latest protonGE from $link? (y/n) " decision
-if [ $decision == "y" ]; then
-	curl -LOJ $(curl -s https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest | grep browser_download_url | cut -d\" -f4 | grep -e .tar.gz) && mv GE-Proton* /tmp
-	dir=~/.steam/root/compatibilitytools.d
+latestVersion=$(echo $link | cut -d/ -f8)
+dir=~/.steam/root/compatibilitytools.d
+echo -n "Your proton version is "
+if [[ $(ls $dir) ]]; then
+	currentVersion=$(ls $dir)
+	echo $currentVersion
+else
+	echo "n/a"
+fi
+if [[ $latestVersion == $currentVersion ]]; then
+	echo "You have the latest version, exiting..."
+	exit 
+fi
+echo
+read -p "Did you want to download the latest protonGE ($latestVersion) from $link? (y/n) " decision
+if [[ $decision == "y" ]]; then
+	curl -LOJ $link && mv GE-Proton* /tmp
 	if [ -d "$dir" ]; then
 		echo "Clearing compatibilitytools.d..."
 		rm -rf $dir/*
